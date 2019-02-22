b2324b2b4c09455338638b22c1a8f9da28548502
sora061 <bhattacharjeeaishik22@gmail.com>
2019-02-22 20:22:42 +0530

    Do-While Infinte Loop
    Signed-off-by: sora061 <bhattacharjeeaishik22@gmail.com>

---
+++ FlowOfControl/DoWhileExample1.java
@@ -0,0 +1,10 @@
+//Program Demonstrating Infinte do-while loop
+public class DoWhileExample1
+{
+	public static void main(String... args)
+	{
+		do{
+			System.out.println("Infinite Loop");
+		}while(true);
+	}
+};
