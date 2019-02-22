cea5dc54fa0d0aec0c6c8e2bc072b980c4bf7070
sora061 <bhattacharjeeaishik22@gmail.com>
2019-02-22 18:47:32 +0530

    Second commit

---
+++ SwitchExample4.java
@@ -0,0 +1,44 @@
+//Nested Switch-Case Statement
+public class SwitchExample4
+{
+	public static void main(String[] args)
+	{
+		char branch = 'C';
+		int collegeYear = 4;
+		switch(collegeYear)
+		{
+			case 1:	System.out.println("English, Mathematics, Science");
+				break;
+			case 2: switch( branch )   
+                		{  
+                    			case 'C': System.out.println("Operating System, Java, Data Structure");  
+                        			  break;  
+                    			case 'E': System.out.println("Micro processors, Logic switching theory");  
+                        			  break;
+					case 'M': System.out.println("Drawing, Manufacturing Machines");  
+                        			  break;  
+                		}  
+                		break;  
+            		case 3: switch( branch )   
+                		{	  
+                    			case 'C': System.out.println("Computer Organization, MultiMedia");  
+                        			  break;  
+                    			case 'E': System.out.println("Fundamentals of Logic Design, Microelectronics");  
+                        			  break;  
+                    			case 'M': System.out.println("Internal Combustion Engines, Mechanical Vibration");  
+                        			  break;  
+                		}  
+                		break;  
+            		case 4: switch( branch )   
+                		{  
+                    			case 'C': System.out.println("Data Communication and Networks, MultiMedia");  
+                        			  break;  
+                    			case 'E': System.out.println("Embedded System, Image Processing");  
+                        			  break;  
+                    			case 'M': System.out.println("Production Technology, Thermal Engineering");  
+                        			  break;
+				}
+				break;
+		}
+	}
+};
---
+++ SwitchExample5.java
@@ -0,0 +1,29 @@
+//Switch-Case Statement to demonstrate the use of Enum
+public class SwitchExample5
+{
+	public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+	public static void main(String[] args)
+	{
+		Day[] DayNow = Day.values();
+		for(Day Now : DayNow)
+		{
+			switch(Now)
+			{
+				case Sunday:	System.out.println("Sunday");
+						break;
+				case Monday:	System.out.println("Monday");
+						break;
+				case Tuesday:	System.out.println("Tuesday");
+						break;
+				case Wednesday:	System.out.println("Wednesday");
+						break;
+				case Thursday:	System.out.println("Thursday");
+						break;
+				case Friday:	System.out.println("Friday");
+						break;
+				case Saturday:	System.out.println("Saturday");
+						break;
+			}
+		}
+	}
+}
